6573044a8f6d88514a2cd40469cb9203e06fde84
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-03-27 05:40:50 +0900

    Update no.js

--- js/no.js
+++ js/no.js
@@ -1,2 +1,10 @@
 document.oncontextmenu = function(){ return false; };
 document.body.oncontextmenu = "return false;"
+
+async function makeKey(){
+	let secret_key = await crypto.subtle.generateKey({name:'AES-GCM', length:256}, true, ['encrypt','decrypt'])
+	let export_secret_key = await crypto.subtle.exportKey('jwk', secret_key)
+	//console.log(export_secret_key)
+	return export_secret_key
+}
+makeKey()
