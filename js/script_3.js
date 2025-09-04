e8536eb51e255e472a6abf2d1ad4a10fd8e1838e
ライフガード <162636336+akaina0112@users.noreply.github.com>
2025-09-04 21:27:18 +0900

    Update script_3.js

--- js/script_3.js
+++ js/script_3.js
@@ -1,59 +1,58 @@
-$(".info").modaal({
-	overlay_close:true,//モーダル背景クリック時に閉じるか
-	before_open:function(){// モーダルが開く前に行う動作
-		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-	},
-	after_close:function(){// モーダルが閉じた後に行う動作
-		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-	}
-});
-	
-//2. 確認を促すモーダル
-$(".confirm").modaal({
-  type:'confirm',
-  confirm_title: 'ログイン画面',//確認画面タイトル
-  confirm_button_text: 'ログイン', //確認画面ボタンのテキスト
-  confirm_cancel_button_text: 'キャンセル',//確認画面キャンセルボタンのテキスト
-  confirm_content: 'ログインが必要です。この画面はボタンを押さなければ閉じません。',//確認画面の内容
-});
+$(document).ready(function(){
 
+  $(".info").modaal({
+    overlay_close:true,
+    before_open:function(){
+      $('html').css('overflow-y','hidden');
+    },
+    after_close:function(){
+      $('html').css('overflow-y','scroll');
+    }
+  });
 
-//3. 画像のモーダル
-$(".gallery").modaal({
-	type: 'image',
-	overlay_close:true,//モーダル背景クリック時に閉じるか
-	before_open:function(){// モーダルが開く前に行う動作
-		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-	},
-	after_close:function(){// モーダルが閉じた後に行う動作
-		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-	}
-});
+  $(".confirm").modaal({
+    type:'confirm',
+    confirm_title: 'ログイン画面',
+    confirm_button_text: 'ログイン',
+    confirm_cancel_button_text: 'キャンセル',
+    confirm_content: 'ログインが必要です。この画面はボタンを押さなければ閉じません。'
+  });
 
-//4. 動画のモーダル
-$(".video-open").modaal({
-	type: 'video',
-	overlay_close:true,//モーダル背景クリック時に閉じるか
-	background: '#28BFE7', // 背景色
-	overlay_opacity:0.8, // 透過具合
-	before_open:function(){// モーダルが開く前に行う動作
-		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-	},
-	after_close:function(){// モーダルが閉じた後に行う動作
-		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-	}
-});
-	
-//5. iframeのモーダル
-$(".iframe-open").modaal({
-		type:'iframe',
-		width: 800,//iframe横幅
-		height:800,//iframe高さ
-		overlay_close:true,//モーダル背景クリック時に閉じるか
-	before_open:function(){// モーダルが開く前に行う動作
-		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-	},
-	after_close:function(){// モーダルが閉じた後に行う動作
-		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-	}
+  $(".gallery").modaal({
+    type: 'image',
+    overlay_close:true,
+    before_open:function(){
+      $('html').css('overflow-y','hidden');
+    },
+    after_close:function(){
+      $('html').css('overflow-y','scroll');
+    }
+  });
+
+  $(".video-open").modaal({
+    type: 'video',
+    overlay_close:true,
+    background: '#28BFE7',
+    overlay_opacity:0.8,
+    before_open:function(){
+      $('html').css('overflow-y','hidden');
+    },
+    after_close:function(){
+      $('html').css('overflow-y','scroll');
+    }
+  });
+
+  $(".iframe-open").modaal({
+    type:'iframe',
+    width: 800,
+    height:800,
+    overlay_close:true,
+    before_open:function(){
+      $('html').css('overflow-y','hidden');
+    },
+    after_close:function(){
+      $('html').css('overflow-y','scroll');
+    }
+  });
+
 });
