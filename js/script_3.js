da8c13a76751b6d83df88659ef449932cd809090
ライフガード <162636336+akaina0112@users.noreply.github.com>
2025-09-04 21:32:48 +0900

    Update script_3.js

--- js/script_3.js
+++ js/script_3.js
@@ -1,57 +1,64 @@
-$(document).ready(function(){
+// script_3.js
+// DOMが準備されたらモーダルを初期化
+$(document).ready(function() {
 
+  // 1. infoモーダル
   $(".info").modaal({
-    overlay_close:true,
-    before_open:function(){
-      $('html').css('overflow-y','hidden');
+    overlay_close: true,
+    before_open: function() {
+      $('html').css('overflow-y', 'hidden'); // 縦スクロールを非表示
     },
-    after_close:function(){
-      $('html').css('overflow-y','scroll');
+    after_close: function() {
+      $('html').css('overflow-y', 'scroll'); // 縦スクロールを表示
     }
   });
 
+  // 2. 確認モーダル
   $(".confirm").modaal({
-    type:'confirm',
+    type: 'confirm',
     confirm_title: 'ログイン画面',
     confirm_button_text: 'ログイン',
     confirm_cancel_button_text: 'キャンセル',
     confirm_content: 'ログインが必要です。この画面はボタンを押さなければ閉じません。'
   });
 
+  // 3. 画像モーダル
   $(".gallery").modaal({
     type: 'image',
-    overlay_close:true,
-    before_open:function(){
-      $('html').css('overflow-y','hidden');
+    overlay_close: true,
+    before_open: function() {
+      $('html').css('overflow-y', 'hidden');
     },
-    after_close:function(){
-      $('html').css('overflow-y','scroll');
+    after_close: function() {
+      $('html').css('overflow-y', 'scroll');
     }
   });
 
+  // 4. 動画モーダル
   $(".video-open").modaal({
     type: 'video',
-    overlay_close:true,
+    overlay_close: true,
     background: '#28BFE7',
-    overlay_opacity:0.8,
-    before_open:function(){
-      $('html').css('overflow-y','hidden');
+    overlay_opacity: 0.8,
+    before_open: function() {
+      $('html').css('overflow-y', 'hidden');
     },
-    after_close:function(){
-      $('html').css('overflow-y','scroll');
+    after_close: function() {
+      $('html').css('overflow-y', 'scroll');
     }
   });
 
+  // 5. iframeモーダル
   $(".iframe-open").modaal({
-    type:'iframe',
+    type: 'iframe',
     width: 800,
-    height:800,
-    overlay_close:true,
-    before_open:function(){
-      $('html').css('overflow-y','hidden');
+    height: 800,
+    overlay_close: true,
+    before_open: function() {
+      $('html').css('overflow-y', 'hidden');
     },
-    after_close:function(){
-      $('html').css('overflow-y','scroll');
+    after_close: function() {
+      $('html').css('overflow-y', 'scroll');
     }
   });
 
